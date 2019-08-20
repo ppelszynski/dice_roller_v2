967cfe2672be8e520e3057ca1bc2ca00bea802d4
Przemek Pelszynski <p.pelszynski@selleo.com>
2019-08-20 17:34:20 +0200

    2. Number filtering test

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 require_relative '../dice'
 
 describe Dice do 
-  it 'generates number from specified range' do
+  it 'generates 5 filtered numbers' do
+    numbers1 = [1, 2, 3, 4, 5, 6, 7]
+    numbers2 = [1, 1, 1, 2, 3, 3, 3]
     dice = Dice.new
 
-    20.times do 
-      expect(dice.roll).to be_between(1, 20)
-    end
+    expect(dice.roll(numbers1)).to eq([2, 3, 4, 5, 6])
+    expect(dice.roll(numbers2)).to eq([1, 1, 2, 3, 3])
   end     
 end
