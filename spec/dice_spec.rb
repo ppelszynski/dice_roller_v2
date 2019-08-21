20ac6ba8a88563fc41a2486619a91d1c9cd51489
Przemek Pelszynski <p.pelszynski@selleo.com>
2019-08-21 10:14:42 +0200

    3. Sum checking and filtering service test

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,11 +3,24 @@ require_relative '../dice'
 
 describe Dice do 
   it 'generates 5 filtered numbers' do
-    numbers1 = [1, 2, 3, 4, 5, 6, 7]
-    numbers2 = [1, 1, 1, 2, 3, 3, 3]
     dice = Dice.new
 
-    expect(dice.roll(numbers1)).to eq([2, 3, 4, 5, 6])
-    expect(dice.roll(numbers2)).to eq([1, 1, 2, 3, 3])
+    expect(dice.roll.length).to eq(5)
+    expect(dice.numbers.length).to eq(7)
+  end     
+
+  it 'filters numbers with sum equal 55' do
+    dice = Dice.new
+    numbers = [1, 5, 10, 10, 15, 15, 19]
+
+    expect(dice.roll(numbers)).to eq([5, 10, 10, 15, 15])
+  end     
+
+  it 'rerolls when sum less than 55' do
+    dice = Dice.new
+    numbers = [0, 0, 0, 0, 0, 0, 0]
+
+    expect(dice.roll(numbers)).not_to eq([0, 0, 0, 0, 0])
+    expect(dice.roll(numbers).length).to eq(5)
   end     
 end
